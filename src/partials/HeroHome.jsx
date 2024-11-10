4f67cda6360aca453332d5efe6b9ed4fc99cc357
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2024-11-10 15:48:21 -0500

    una de

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -30,7 +30,7 @@ function HeroHome() {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-            Sistema de Gestión de Registro de Contratos de <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">de la Dirección General de Servicios</span>
+            Sistema de Gestión de Registro de Contratos de <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400"> la Dirección General de Servicios</span>
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
