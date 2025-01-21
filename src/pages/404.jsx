ff539a8ec493178783fb10ebcaa4be7d260ad747
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-01-21 01:16:07 -0500

    se agrego la pagina 404 con stilos

---
+++ src/pages/404.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+  const{auth}=useAuth();
+
+  const handleGoHome = () => {
+    if(auth.tipo_usuario==="Admin_Gnl"){
+      navigate('/admin/registro-contrato');
+    }
+    if(auth.tipo_usuario==="director"){
+      navigate('/directivo/registro-contrato');
+    }
+    if(auth.tipo_usuario==="especialista"){
+      navigate('/especialista/registro-contrato');
+    }else{
+        navigate('/');
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-teal-500 animate-fadeIn">
+      <h1 className="text-6xl font-bold text-white mb-4 animate-jump">404</h1>
+      <h2 className="text-4xl font-bold text-white animate-typing overflow-hidden whitespace-nowrap border-r-4 border-white">Página No Encontrada</h2>
+      <p className="mt-4 text-white animate-slideIn">Lo sentimos, la página que estás buscando no existe.</p>
+      <button
+        onClick={handleGoHome}
+        className="mt-6 px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-200 transition duration-300 ease-in-out transform hover:scale-105 animate-bounce"
+      >
+        Regresar al Home
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
