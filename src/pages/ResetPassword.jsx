70fa8f2968d3dc402b91a4e1269a8c46afa42e43
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-15 23:54:51 -0500

    dark mode responsive y animacion y correcion de validaciones

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -5,6 +5,7 @@ import useValidation from "../hooks/useValidation";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import clienteAxios from "../axios/axios";
+
 const ResetPassword = () => {
   const { validarInput } = useValidation();
   const [email, setEmail] = useState("");
@@ -31,27 +32,27 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
+    <div className="flex flex-col min-h-screen overflow-hidden ">
+      {/* Site header */}
       <Header />
 
-      {/*  Page content */}
-      <main className="flex-grow">
-        <section className="bg-transparent">
+      {/* Page content */}
+      <main className="flex-grow flex items-center justify-center">
+        <section className="w-full">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1 mb-4">
-                  Vamos a ayudarte a<br></br>
+                <h1 className="h1 mb-4 text-black dark:text-white">
+                  Vamos a ayudarte a <br />
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                     recuperar tu cuenta
                   </span>
                 </h1>
-                <p className="text-xl text-gray-600">
+                <p className="text-xl text-gray-600 dark:text-gray-300">
                   Ingrese la dirección de correo electrónico asociada a su
                   cuenta y le enviaremos un enlace para restablecer su
-                  contraseña
+                  contraseña.
                 </p>
               </div>
 
@@ -61,17 +62,19 @@ const ResetPassword = () => {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
+                        className="block text-gray-800 dark:text-white text-sm font-medium mb-1"
                         htmlFor="email"
                       >
-                        Correo electronico{" "}
-                        <span className={ email ? 'text-black':`text-red-600`}>*</span>
+                        Correo electrónico{" "}
+                        <span className={email ? "text-black" : "text-red-600"}>
+                          *
+                        </span>
                       </label>
                       <input
                         id="email"
                         type="email"
                         className="form-input w-full text-gray-800"
-                        placeholder="Intruduzca su dirección de correo electronico"
+                        placeholder="Introduzca su dirección de correo electrónico"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                       />
