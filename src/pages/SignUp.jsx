0d81a872ee229a805dbe45616ef0cd85d0daca2d
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-15 23:53:59 -0500

    mejora en el reponsive y implement del modo oscuro se quitaron detalles

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../partials/headers/Header";
 import useValidation from "../hooks/useValidation";
-import { useState } from "react";
 import clienteAxios from "../axios/axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -41,7 +40,7 @@ const SignUp = () => {
           password,
           tipo_usuario: "visitante",
         });
-        
+
         toast.success(respuesta.data.msg);
         setTimeout(() => {
           setNombre("");
@@ -57,21 +56,20 @@ const SignUp = () => {
     } else {
       try {
         const respuesta = await clienteAxios.post("/usuario", {
-          nombre:nombre.trim(),
-          email:email.trim(),
-          password:password.trim(),
+          nombre: nombre.trim(),
+          email: email.trim(),
+          password: password.trim(),
         });
 
         toast.success(respuesta.data.msg);
-        setTimeout(()=>{
+        setTimeout(() => {
           setNombre("");
-        setEmail("");
-        setPassword("");
-        setRepetirPassword("");
-        setCheck(false);
-        navigate("/auth/signin");
-        },1000)
-        
+          setEmail("");
+          setPassword("");
+          setRepetirPassword("");
+          setCheck(false);
+          navigate("/auth/signin");
+        }, 1000);
       } catch (error) {
         toast.error(error.response.data.msg);
       }
@@ -80,18 +78,18 @@ const SignUp = () => {
 
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
+      {/* Site header */}
       <Header />
 
-      {/*  Page content */}
-      <main className="flex-grow">
-        <section className="bg-transparent">
+      {/* Page content */}
+      <main className="flex-grow flex items-center justify-center">
+        <section className="w-full">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="pt-20 pb-12">
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h1 className="h1">
-                  Tu espacio personalizado <br></br>
+                <h1 className="h1 text-black dark:text-white">
+                  Tu espacio personalizado <br />
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                     Comienza a gestionar contratos de manera intuitiva
                   </span>
@@ -104,10 +102,15 @@ const SignUp = () => {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
+                        className="block text-gray-800 dark:text-white text-sm font-medium mb-1"
                         htmlFor="nombre"
                       >
-                        Nombre <span className={ nombre ? 'text-black':`text-red-600`}>*</span>
+                        Nombre{" "}
+                        <span
+                          className={nombre ? "text-black" : "text-red-600"}
+                        >
+                          *
+                        </span>
                       </label>
                       <input
                         id="nombre"
@@ -125,11 +128,15 @@ const SignUp = () => {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
+                        className="block text-gray-800 dark:text-white text-sm font-medium mb-1"
                         htmlFor="email"
                       >
                         Correo electrónico{" "}
-                        <span className={ email ? 'text-black':`text-red-600`}>*</span>
+                        <span
+                          className={email ? "text-black " : "text-red-600"}
+                        >
+                          *
+                        </span>
                       </label>
                       <input
                         id="email"
@@ -147,16 +154,21 @@ const SignUp = () => {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
+                        className="block text-gray-800 dark:text-white text-sm font-medium mb-1"
                         htmlFor="password"
                       >
-                        Contraseña <span className={ password ? 'text-black':`text-red-600`}>*</span>
+                        Contraseña{" "}
+                        <span
+                          className={password ? "text-black" : "text-red-600"}
+                        >
+                          *
+                        </span>
                       </label>
                       <input
                         id="password"
                         type="password"
                         className="form-input w-full text-gray-800"
-                        placeholder="Introduzca su contraseña "
+                        placeholder="Introduzca su contraseña"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                       />
@@ -168,11 +180,17 @@ const SignUp = () => {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
+                        className="block text-gray-800 dark:text-white text-sm font-medium mb-1"
                         htmlFor="repetirpassword"
                       >
                         Repetir contraseña{" "}
-                        <span className={ repetirPassword ? 'text-black':`text-red-600`}>*</span>
+                        <span
+                          className={
+                            repetirPassword ? "text-black" : "text-red-600"
+                          }
+                        >
+                          *
+                        </span>
                       </label>
                       <input
                         id="repetirpassword"
@@ -189,7 +207,7 @@ const SignUp = () => {
                       </span>
                     )}
                   </div>
-                  
+
                   <div className="flex flex-wrap -mx-3 mt-6">
                     <div className="w-full px-3">
                       <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
@@ -199,13 +217,13 @@ const SignUp = () => {
                   </div>
                 </form>
 
-                <div className="text-gray-600 text-center mt-6">
-                  ¿Ya tengo una cuenta?{" "}
+                <div className="text-gray-800 dark:text-white text-center mt-6">
+                  ¿Ya tienes una cuenta?{" "}
                   <Link
                     to="/auth/signin"
-                    className="text-blue-600 hover:underline transition duration-150 ease-in-out"
+                    className="text-blue-400 hover:underline transition duration-150 ease-in-out"
                   >
-                    Iniciar sesion
+                    Iniciar sesión
                   </Link>
                 </div>
               </div>
