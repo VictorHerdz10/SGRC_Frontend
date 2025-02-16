fe1e498e919f5eb2a432be7a2cf855db9466f712
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-16 00:00:14 -0500

    se aagrefo responsi y clases apra el uso generico  y se elimino bg image

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,13 +3,7 @@ import imageUrl from "../images/register_bg_2.png";
 const AuthLayout = () => {
 
   return (
-    <div className="container mx-auto px-4 py-5 absolute inset-0  w-full h-full bg-blueGray-800 bg-no-repeat bg-cover bg-center min-w-screen-md"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}>
-
+    <div className="w-full h-full min-w-screen-md dark:bg-uci">
       <Outlet />
 
     </div>
