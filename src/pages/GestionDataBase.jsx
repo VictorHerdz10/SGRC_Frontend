361741aff5a33c5291c57e867d8867f7a4cb67d8
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-15 23:58:26 -0500

    se acomodo los botones y los componentes mostrados en el backup

--- src/pages/GestionDataBase.jsx
+++ src/pages/GestionDataBase.jsx
@@ -1,6 +1,6 @@
 import BackupComponent from "../components/others/Backup";
 import AsideAdmin from "../components/aside/AsideAdmin";
-import HeaderAdmin from '../partials/headers/HeaderAuth';
+import HeaderAdmin from "../partials/headers/HeaderAuth";
 import useValidation from "../hooks/useValidation";
 
 const GestionDataBase = () => {
@@ -13,10 +13,10 @@ const GestionDataBase = () => {
           <AsideAdmin />
         </div>
         <div className="flex flex-col flex-1 min-w-0">
-          <div className="sticky top-0 w-full p-4 bg-white dark:bg-gray-900 bg-opacity-10 z-10 mb-6">
+          <div className="sticky top-0 w-full p-4 bg-opacity-10 z-10 mb-6">
             <HeaderAdmin />
           </div>
-          <div className="flex-1 p-6 bg-white dark:bg-gray-900 overflow-y-auto">
+          <div className="flex-1 p-6 bg-white dark:bg-uci overflow-y-auto">
             <BackupComponent />
           </div>
         </div>
