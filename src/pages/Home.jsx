c2cac08938fbde7610c709f0f2e0979eac82f5ae
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-15 23:55:41 -0500

    se acodo el home enfuncion del layaout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../partials/headers/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Footer from '../partials/footer/Footer';
+import Header from "../partials/headers/Header";
+import HeroHome from "../partials/HeroHome";
+import FeaturesBlocks from "../partials/FeaturesBlocks";
+import Footer from "../partials/footer/Footer";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
+    <div className="flex flex-col min-h-screen overflow-hidden bg-gray-100 dark:bg-uci ">
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         {/*  Page sections */}
         <HeroHome />
         <FeaturesBlocks />
-
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
