dd7d86fe72f322bea5b7a7debdc9c7241cb3102f
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-16 00:02:58 -0500

    funtes cambiadas mejoras de colores responsive y dark mode

--- src/components/table/TablaEntidad.jsx
+++ src/components/table/TablaEntidad.jsx
@@ -7,7 +7,7 @@ import {
   FaEdit,
   FaUserPlus,
 } from "react-icons/fa";
-import {toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { IoClose } from "react-icons/io5";
 import clienteAxios from "../../axios/axios";
@@ -40,14 +40,14 @@ const PanelEntidad = () => {
         Authorization: `Bearer ${token}`,
       },
     };
-    
+
     try {
       if (token && auth) {
-      const url = `entidad/eliminar-entidad/${id}`;
-      const response = await clienteAxios.delete(url, config);
-      setShowComfirmDelete(false);
-      obtenerEntidades();
-      toast.success(response.data.msg);
+        const url = `entidad/eliminar-entidad/${id}`;
+        const response = await clienteAxios.delete(url, config);
+        setShowComfirmDelete(false);
+        obtenerEntidades();
+        toast.success(response.data.msg);
       }
     } catch (error) {
       toast.error(error.response.data.msg);
@@ -79,15 +79,15 @@ const PanelEntidad = () => {
           config
         );
         setShowModal(false);
-        setNewEntidad('');
+        setNewEntidad("");
         toast.success(respuesta.data.msg);
-            obtenerEntidades();
-        
+        obtenerEntidades();
       }
     } catch (error) {
       toast.error(error.response.data.msg);
     }
   };
+
   const handleUpdate = async (e) => {
     e.preventDefault();
     const errores = validarInput(entidadUpdate, "text", "");
@@ -110,11 +110,9 @@ const PanelEntidad = () => {
         config
       );
       setShowModalUpdate(false);
-      setEntidadUpdate('');
+      setEntidadUpdate("");
       toast.success(respuesta.data.msg);
-      
-        obtenerEntidades();
-    
+      obtenerEntidades();
     } catch (error) {
       console.error("Error al actualizar rol:", error.message);
       toast.error(error.response.data.msg);
@@ -123,53 +121,59 @@ const PanelEntidad = () => {
 
   return (
     <>
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4 dark:text-white">
         <div>
-          <h2 className="bg-clip-text text-xl font-semibold mb-4 text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+          <h2 className="bg-clip-text text-2xl font-semibold mb-4 text-transparent bg-gradient-to-r from-blue-500 to-teal-400 dark:from-blue-300 dark:to-teal-200">
             Entidades existentes
           </h2>
           <div className="overflow-x-auto">
-            <table className="min-w-full bg-white">
+            <table className="min-w-full bg-white dark:bg-gray-800">
               <thead>
                 <tr>
-                  <th className="py-2 px-4 border-l border-b border-r">No.</th>
-                  <th className="py-2 px-4 border-l border-b border-r">
+                  <th className="py-2 px-4 border-l border-b border-r dark:border-gray-700">
+                    No.
+                  </th>
+                  <th className="py-2 px-4 border-l border-b border-r dark:border-gray-700">
                     Entidad
                   </th>
-                  <th className="py-2 px-4 border-b border-r">
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
                     Creado Por
-                    </th> 
-                  <th className="py-2 px-4 border-b border-r">
+                  </th>
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
                     Fecha de Creado
                   </th>
-                  <th className="py-2 px-4 border-b border-r ">
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
                     Fecha de Modificado
                   </th>
-                  <th className="py-2 px-4 border-b border-r ">Acciones</th>
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
+                    Acciones
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 {entidades.map((entidad, index) => (
-                  <tr key={entidad._id}>
-                    <td className="py-2 border-l px-6 border-b border-r ">
+                  <tr key={entidad._id} className="dark:bg-gray-700">
+                    <td className="py-2 border-l px-6 border-b border-r dark:border-gray-600">
                       {index + 1}
                     </td>
-                    <td className="py-2 px-4 border-b border-r ">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       {entidad.entidad}
                     </td>
-                    <td className="py-2 px-4 border-b border-r">
-                      {auth.nombre===entidad.nombreEjecutivo? `${entidad.nombreEjecutivo} (Yo)`:entidad.nombreEjecutivo}
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
+                      {auth.nombre === entidad.nombreEjecutivo
+                        ? `${entidad.nombreEjecutivo} (Yo)`
+                        : entidad.nombreEjecutivo}
                     </td>
-                    <td className="py-2 px-4 border-b border-r text-center">
+                    <td className="py-2 px-4 border-b border-r text-center dark:border-gray-600">
                       {parcearDate(new Date(entidad.creado))}
                     </td>
-                    <td className="py-2 px-4 border-b border-r text-center">
+                    <td className="py-2 px-4 border-b border-r text-center dark:border-gray-600">
                       {parcearDate(new Date(entidad.modificado))}
                     </td>
-                    <td className="py-2 px-4 border-b border-r">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       <div className="flex space-x-2 justify-center">
                         <FaEdit
-                          className="text-blue-500 cursor-pointer"
+                          className="text-blue-500 cursor-pointer dark:text-blue-300"
                           onClick={() => {
                             setShowModalUpdate(true);
                             setEntidadId(entidad._id);
@@ -177,7 +181,7 @@ const PanelEntidad = () => {
                           }}
                         />
                         <FaTrash
-                          className="text-red-500 cursor-pointer"
+                          className="text-red-500 cursor-pointer dark:text-red-300"
                           onClick={() => {
                             setShowComfirmDelete(true);
                             setEntidadId(entidad._id);
@@ -189,10 +193,10 @@ const PanelEntidad = () => {
                 ))}
               </tbody>
             </table>
-            <div className=" flex justify-items-start">
+            <div className="flex justify-items-start">
               <button
                 onClick={() => setShowModal(true)}
-                className="flex items-center mt-3 space-x-1 px-1 py-1 bg-blue-600 text-white rounded hover:bg-blue-800"
+                className="flex items-center mt-3 space-x-1 px-1 py-1 bg-blue-600 text-white rounded hover:bg-blue-800 dark:bg-blue-700 dark:hover:bg-blue-900"
               >
                 <FaPlus className="h-3 w-2" />
                 <span className="text-sm text-white">
@@ -204,31 +208,32 @@ const PanelEntidad = () => {
         </div>
         {showConfirmDelete && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn">
+            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn dark:bg-gray-800">
               <button
                 onClick={() => setShowComfirmDelete(false)}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors"
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors dark:text-gray-400 dark:hover:text-gray-200"
                 aria-label="Close confirmation"
               >
                 <IoClose size={24} />
               </button>
-              <h2 className="text-xl font-bold mb-4">Advertencia</h2>
-              <p className="text-gray-600 mb-6">
+              <h2 className="text-xl font-bold mb-4 dark:text-white">
+                Advertencia
+              </h2>
+              <p className="text-gray-600 mb-6 dark:text-gray-300">
                 ¿Estás seguro de que deseas eliminar esta entidad?
               </p>
               <div className="flex justify-end space-x-4">
                 <button
                   onClick={() => setShowComfirmDelete(false)}
-                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors"
+                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors dark:text-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
-                  Cancel
+                  Cancelar
                 </button>
                 <button
-                  onClick={() =>{handleDeleteEntidad(entidadId);
-                    
-                    
+                  onClick={() => {
+                    handleDeleteEntidad(entidadId);
                   }}
-                  className="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-colors"
+                  className="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-colors dark:bg-red-500 dark:hover:bg-red-600"
                 >
                   Eliminar
                 </button>
@@ -238,16 +243,29 @@ const PanelEntidad = () => {
         )}
         {showModal && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-lg w-96 animate-slideIn">
-              <h3 className="text-lg font-semibold mb-4">
+            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn dark:bg-gray-800">
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                  setErrorText("");
+                  setEntidadUpdate("");
+                  setEntidadId("");
+                  setNewEntidad("");
+                }}
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors dark:text-gray-400 dark:hover:text-gray-200"
+                aria-label="Close confirmation"
+              >
+                <IoClose size={24} />
+              </button>
+              <h3 className="text-lg font-semibold mb-4 dark:text-white">
                 Registrar una nueva entidad
               </h3>
               <div className="mb-4">
                 <label
                   htmlFor="entidad"
-                  className="block text-gray-700 text-sm font-semibold mb-1"
+                  className="block text-gray-700 text-sm font-semibold mb-1 dark:text-gray-300"
                 >
-                 Entidad
+                  Entidad
                 </label>
                 <input
                   type="text"
@@ -256,9 +274,13 @@ const PanelEntidad = () => {
                   placeholder="Entidad"
                   value={newEntidad}
                   onChange={(e) => setNewEntidad(e.target.value)}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-white dark:border-gray-600"
                 />
-                {errorText && <span className="text-red-500">{errorText}</span>}
+                {errorText && (
+                  <span className="text-red-500 dark:text-red-400">
+                    {errorText}
+                  </span>
+                )}
               </div>
               <div className="flex justify-between space-x-4 mt-5">
                 <button
@@ -267,15 +289,15 @@ const PanelEntidad = () => {
                     setErrorText("");
                     setEntidadUpdate("");
                     setEntidadId("");
-                    setNewEntidad('');
+                    setNewEntidad("");
                   }}
-                  className="px-10 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors"
+                  className="px-10 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors dark:text-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
                   Cancelar
                 </button>
                 <button
                   onClick={(e) => handleCrearEntidad(e)}
-                  className="px-10 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors"
+                  className="px-10 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors dark:bg-blue-500 dark:hover:bg-blue-600"
                 >
                   Registrar
                 </button>
@@ -285,26 +307,27 @@ const PanelEntidad = () => {
         )}
         {showModalUpdate && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn">
+            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn dark:bg-gray-800">
               <button
-                onClick={() => {setShowModalUpdate(false)
-                    setErrorText("");
-                    setEntidadUpdate("");
-                    setEntidadId("");
-                    setNewEntidad('');
+                onClick={() => {
+                  setShowModalUpdate(false);
+                  setErrorText("");
+                  setEntidadUpdate("");
+                  setEntidadId("");
+                  setNewEntidad("");
                 }}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors"
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors dark:text-gray-400 dark:hover:text-gray-200"
                 aria-label="Close confirmation"
               >
                 <IoClose size={24} />
               </button>
-              <h3 className="text-lg font-semibold mb-4">
+              <h3 className="text-lg font-semibold mb-4 dark:text-white">
                 Actualizar Entidad
               </h3>
               <div className="mb-4">
                 <label
                   htmlFor="entidad"
-                  className="block text-gray-700 text-sm font-semibold mb-1"
+                  className="block text-gray-700 text-sm font-semibold mb-1 dark:text-gray-300"
                 >
                   Entidad
                 </label>
@@ -315,9 +338,13 @@ const PanelEntidad = () => {
                   placeholder="Entidad"
                   value={entidadUpdate}
                   onChange={(e) => setEntidadUpdate(e.target.value)}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-gray-700 dark:text-white dark:border-gray-600"
                 />
-                {errorText && <span className="text-red-500">{errorText}</span>}
+                {errorText && (
+                  <span className="text-red-500 dark:text-red-400">
+                    {errorText}
+                  </span>
+                )}
               </div>
               <div className="flex justify-end space-x-4 mt-5">
                 <button
@@ -326,15 +353,15 @@ const PanelEntidad = () => {
                     setErrorText("");
                     setEntidadUpdate("");
                     setEntidadId("");
-                    setNewEntidad('');
+                    setNewEntidad("");
                   }}
-                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors"
+                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors dark:text-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
                   Cancelar
                 </button>
                 <button
                   onClick={(e) => handleUpdate(e)}
-                  className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors"
+                  className="px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors dark:bg-blue-500 dark:hover:bg-blue-600"
                 >
                   Actualizar
                 </button>
