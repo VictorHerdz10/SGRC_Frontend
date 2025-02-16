13fafd9484524f2bef61dc3d712b6155a40bd5a1
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-16 00:00:34 -0500

    se adapto a las demas componetes para el uso del responsive

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -22,7 +22,7 @@ const RutaProtegida = () => {
   if (cargando) return "cargando...";
 
   return (
-    <div className="min-w-screen-md">
+    <div className="min-w-screen-md dark:bg-uci">
       <>{auth?._id ? <Outlet /> : <Navigate to="/auth/signin" />}</>
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
