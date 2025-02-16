35ff9788a1f3be6fb0982f3209adea1e716469d1
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-15 23:57:38 -0500

    se agregacron cases faltantes responsive y dark mode

--- src/pages/GestionEntidad.jsx
+++ src/pages/GestionEntidad.jsx
@@ -1,36 +1,37 @@
 import React, { useState } from "react";
-import HeaderAuth from "../partials/headers/HeaderAuth"
-import AsideAdmin from '../components/aside/AsideAdmin'
+import HeaderAuth from "../partials/headers/HeaderAuth";
+import AsideAdmin from "../components/aside/AsideAdmin";
 import useValidation from "../hooks/useValidation";
 import useAuth from "../hooks/useAuth";
-import AsideDirector from '../components/aside/AsideDirector';
+import AsideDirector from "../components/aside/AsideDirector";
 import PanelEntidad from "../components/table/TablaEntidad";
 
 const GestionEntidad = () => {
-    const{isOpen}=useValidation();
-    const {auth} = useAuth();
-    
-  
-    return (
-  <>
-  
-  <div className="flex">
-    <div className={  isOpen ? "p-4 ml-10 w-64" : "p-4 w-20" }>
-    {auth.tipo_usuario==='Admin_Gnl' ? <AsideAdmin/>:<AsideDirector/> }
-    </div>
-    <div className="w-full flex flex-col">
-      <div className="w-full p-4  ">
-        <HeaderAuth/>
+  const { isOpen } = useValidation();
+  const { auth } = useAuth();
+
+  return (
+    <>
+      <div className="flex h-screen overflow-hidden">
+        <div className={isOpen ? "p-4 ml-10 w-64" : "p-4 w-20"}>
+          {auth.tipo_usuario === "Admin_Gnl" ? (
+            <AsideAdmin />
+          ) : (
+            <AsideDirector />
+          )}
+        </div>
+        <div className="w-full flex flex-col">
+          <div className="w-full p-4  ">
+            <HeaderAuth />
+          </div>
+
+          <div className="w-full p-10 ml-0 mt-20 col-span-10 bg-white dark:bg-uci overflow-y-auto">
+            <PanelEntidad />
+          </div>
+        </div>
       </div>
-      
-      <div className="w-full p-10 ml-0 mt-20 col-span-10 bg-white dark:bg-gray-900 overflow-y-auto">
-       <PanelEntidad/>
-      </div>
-    </div>
-  </div>
-       </>   
-      
-    );
-  };
-  
-  export default GestionEntidad;
+    </>
+  );
+};
+
+export default GestionEntidad;
