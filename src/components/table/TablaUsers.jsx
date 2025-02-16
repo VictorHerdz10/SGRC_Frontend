3478bc88b2fd8a86aeb60d978e21a00efe718fc3
VictorHerdz10 <victorhernandezsalcedo4@gmail.com>
2025-02-16 00:01:17 -0500

    responsive mejorado se agragron validaciones colores y fonts

--- src/components/table/TablaUsers.jsx
+++ src/components/table/TablaUsers.jsx
@@ -24,7 +24,7 @@ const PanelUsuario = () => {
   const [showModal, setShowModal] = useState(false);
   const [directores, setDirectores] = useState([]);
   const [selectedDirector, setSelectedDirector] = useState("");
-  const{users,obtenerUsuarios} =useValidation();
+  const { users, obtenerUsuarios } = useValidation();
 
   const handleDirectorChange = (directorId) => {
     setSelectedDirector(directorId);
@@ -80,12 +80,11 @@ const PanelUsuario = () => {
     const url = `usuario/eliminar-usuario/${userId}`;
     try {
       const respuesta = await clienteAxios.delete(url, config);
-      
 
       setShowComfirmDelete(false);
       setUserId("");
       toast.success(respuesta.data.msg);
-        obtenerUsuarios();
+      obtenerUsuarios();
     } catch (error) {
       toast.success(error.response.data.msg);
     }
@@ -101,7 +100,7 @@ const PanelUsuario = () => {
         Authorization: `Bearer ${token}`,
       },
     };
-    
+
     try {
       const userToModify = users.find((user) => user._id === userId);
       if (!userToModify) {
@@ -117,7 +116,7 @@ const PanelUsuario = () => {
         );
         setShowComfirmUpdate(false);
         toast.success(respuesta.data.msg);
-          obtenerUsuarios();
+        obtenerUsuarios();
       }
     } catch (error) {
       console.error("Error al asignar rol:", error.message);
@@ -127,58 +126,67 @@ const PanelUsuario = () => {
 
   return (
     <>
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4 dark:text-white">
         <div>
-          <h2 className="bg-clip-text text-xl font-semibold mb-4 text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+          <h2 className="bg-clip-text text-2xl font-semibold mb-4 text-transparent bg-gradient-to-r from-blue-500 to-teal-400 dark:from-blue-300 dark:to-teal-200">
             Usuarios existentes
           </h2>
           <div className="overflow-x-auto">
-            <table className="min-w-full bg-white">
+            <table className="min-w-full bg-white dark:bg-gray-800">
               <thead>
                 <tr>
-                  <th className="py-2 px-4 border-l border-b border-r">No.</th>
-                  <th className="py-2 px-4 border-l border-b border-r">
+                  <th className="py-2 px-4 border-l border-b border-r dark:border-gray-700">
+                    No.
+                  </th>
+                  <th className="py-2 px-4 border-l border-b border-r dark:border-gray-700">
                     Nombre
                   </th>
-                  <th className="py-2 px-4 border-b border-r">
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
                     Correo electrónico
                   </th>
-                  <th className="py-2 px-4 border-b border-r">Rol</th>
-                  <th className="py-2 px-4 border-b border-r ">Acciones</th>
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
+                    Rol
+                  </th>
+                  <th className="py-2 px-4 border-b border-r dark:border-gray-700">
+                    Acciones
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 {users.map((user, index) => (
-                  <tr key={user._id}>
-                    <td className="py-2 border-l px-6 border-b border-r ">
+                  <tr
+                    key={user._id}
+                    className="hover:bg-gray-50 dark:hover:bg-gray-700"
+                  >
+                    <td className="py-2 border-l px-6 border-b border-r dark:border-gray-600">
                       {index + 1}
                     </td>
-                    <td className="py-2 px-4 border-b border-r ">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       {user.nombre}
                     </td>
-                    <td className="py-2 px-4 border-b border-r">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       {user.email}
                     </td>
-                    <td className="py-2 px-4 border-b border-r">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       {user.tipo_usuario
                         ? getUserRoleText(user.tipo_usuario)
                         : ""}
                     </td>
-                    <td className="py-2 px-4 border-b border-r">
+                    <td className="py-2 px-4 border-b border-r dark:border-gray-600">
                       <div className="flex space-x-2">
                         <FaEye
-                          className="text-blue-500 cursor-pointer"
+                          className="text-blue-500 cursor-pointer dark:text-blue-400"
                           onClick={() => handleViewUser(user._id)}
                         />
                         <FaTrash
-                          className="text-red-500 cursor-pointer"
+                          className="text-red-500 cursor-pointer dark:text-red-400"
                           onClick={() => {
                             setShowComfirmDelete(true);
                             setUserId(user._id);
                           }}
                         />
                         <FaUserPlus
-                          className="text-purple-500 cursor-pointer"
+                          className="text-purple-500 cursor-pointer dark:text-purple-400"
                           onClick={() => {
                             setShowComfirmUpdate(true);
                             setUserId(user._id);
@@ -201,28 +209,30 @@ const PanelUsuario = () => {
         </div>
         {showConfirmDelete && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn">
+            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn dark:bg-gray-800">
               <button
                 onClick={() => setShowComfirmDelete(false)}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors"
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors dark:text-gray-400 dark:hover:text-gray-200"
                 aria-label="Close confirmation"
               >
                 <IoClose size={24} />
               </button>
-              <h2 className="text-xl font-bold mb-4">Advertencia</h2>
-              <p className="text-gray-600 mb-6">
+              <h2 className="text-xl font-bold mb-4 dark:text-white">
+                Advertencia
+              </h2>
+              <p className="text-gray-600 mb-6 dark:text-gray-300">
                 ¿Estás seguro de que deseas eliminar este usuario?
               </p>
               <div className="flex justify-end space-x-4">
                 <button
                   onClick={() => setShowComfirmDelete(false)}
-                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors"
+                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors dark:text-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
-                  Cancel
+                  Cancelar
                 </button>
                 <button
                   onClick={() => handleDeleteUser(userId)}
-                  className="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-colors"
+                  className="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-colors dark:bg-red-500 dark:hover:bg-red-600"
                 >
                   Eliminar
                 </button>
@@ -232,49 +242,56 @@ const PanelUsuario = () => {
         )}
         {showModal && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-lg w-96 animate-slideIn">
-              <h3 className="text-lg font-semibold mb-4">
+            <div className="bg-white p-6 rounded-lg w-96 animate-slideIn dark:bg-gray-800">
+              <h3 className="text-lg font-semibold mb-4 dark:text-white">
                 Información del usuario
               </h3>
-              <h1 className=" flex font-bold text-black">
+              <h1 className="flex font-bold text-black dark:text-white">
                 Nombre:{" "}
-                <p className="text-start text-gray-700 mx-2">{actual.nombre}</p>{" "}
+                <p className="text-start text-gray-700 mx-2 dark:text-gray-300">
+                  {actual.nombre}
+                </p>{" "}
               </h1>
-              <h1 className="flex font-bold text-black">
-                Email:<p className="text-gray-700 mx-2">{actual.email}</p>
+              <h1 className="flex font-bold text-black dark:text-white">
+                Email:
+                <p className="text-gray-700 mx-2 dark:text-gray-300">
+                  {actual.email}
+                </p>
               </h1>
-              <h1 className="flex font-bold text-black">
+              <h1 className="flex font-bold text-black dark:text-white">
                 Rol:
-                <p className="text-gray-700 mx-2">
+                <p className="text-gray-700 mx-2 dark:text-gray-300">
                   {actual.tipo_usuario === "especialista"
                     ? "Especialista"
                     : actual.tipo_usuario === "director"
                     ? "Director"
-                    : "Administrador de sistema" || "Sin Asignar"}
+                    : actual.tipo_usuario === "Admin_Gnl"
+                    ? "Administrador de sistema"
+                    : "Sin Asignar"}
                 </p>
               </h1>
-              <h1 className="flex font-bold text-black">
+              <h1 className="flex font-bold text-black dark:text-white">
                 Telefono:
-                <p className="text-gray-700 mx-2">
+                <p className="text-gray-700 mx-2 dark:text-gray-300">
                   {actual.telefono ? actual.telefono : "-"}
                 </p>
               </h1>
 
-              <h1 className="flex font-bold text-black">
+              <h1 className="flex font-bold text-black dark:text-white">
                 Cargo:
-                <p className="text-gray-700 mx-2">
+                <p className="text-gray-700 mx-2 dark:text-gray-300">
                   {actual.cargo ? actual.cargo : "-"}
                 </p>
               </h1>
 
-              <h1 className="flex font-bold text-black">
+              <h1 className="flex font-bold text-black dark:text-white">
                 Registrado:
-                <p className="text-gray-700 mx-2">
+                <p className="text-gray-700 mx-2 dark:text-gray-300">
                   {actual.creado ? parcearDate(new Date(actual.creado)) : "-"}
                 </p>
               </h1>
               <button
-                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700"
                 onClick={() => {
                   setShowModal(false);
                   setUserId("");
@@ -287,21 +304,23 @@ const PanelUsuario = () => {
         )}
         {showConfirmUpdate && (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn">
+            <div className="relative w-full max-w-md p-6 bg-white rounded-lg shadow-xl animate-slideIn dark:bg-gray-800">
               <button
                 onClick={() => {
                   setRol("");
                   setSelectedDirector("");
                   setShowComfirmUpdate(false);
                 }}
-                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors"
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 transition-colors dark:text-gray-400 dark:hover:text-gray-200"
                 aria-label="Cerrar confirmación"
               >
                 <IoClose size={24} />
               </button>
-              <h2 className="text-xl font-bold mb-4">Asignar Rol</h2>
+              <h2 className="text-xl font-bold mb-4 dark:text-white">
+                Asignar Rol
+              </h2>
 
-              <p className="text-gray-600 mb-6">
+              <p className="text-gray-600 mb-6 dark:text-gray-300">
                 Seleccione el rol para asignarlo al usuario:
               </p>
 
@@ -309,7 +328,7 @@ const PanelUsuario = () => {
                 name="rol"
                 value={rol}
                 onChange={(e) => setRol(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4 dark:bg-gray-700 dark:text-white dark:border-gray-600"
               >
                 <option value="">Seleccione un rol</option>
                 <option value="director">Director</option>
@@ -319,7 +338,7 @@ const PanelUsuario = () => {
 
               {rol === "especialista" && (
                 <div className="mt-4">
-                  <p className="text-sm text-gray-500 mb-2">
+                  <p className="text-sm text-gray-500 mb-2 dark:text-gray-400">
                     Para obtener permisos, debe vincular al especialista con un
                     director:
                   </p>
@@ -331,7 +350,7 @@ const PanelUsuario = () => {
                       >
                         <input
                           type="radio"
-                          id={"radio-${director._id}"}
+                          id={`radio-${director._id}`}
                           name="director"
                           value={director._id}
                           checked={selectedDirector === director._id}
@@ -340,7 +359,7 @@ const PanelUsuario = () => {
                         />
                         <label
                           htmlFor={`radio-${director._id}`}
-                          className="text-sm"
+                          className="text-sm dark:text-gray-300"
                         >
                           {director.nombre}
                         </label>
@@ -357,9 +376,9 @@ const PanelUsuario = () => {
                     setSelectedDirector("");
                     setShowComfirmUpdate(false);
                   }}
-                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors"
+                  className="px-4 py-2 text-gray-600 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors dark:text-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
                 >
-                  Cancel
+                  Cancelar
                 </button>
                 <button
                   onClick={(e) => {
@@ -367,7 +386,7 @@ const PanelUsuario = () => {
                     handleSubmit;
                   }}
                   disabled={rol === "especialista" && !selectedDirector}
-                  className="px-4 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700 transition-colors"
+                  className="px-4 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700 transition-colors dark:bg-green-500 dark:hover:bg-green-600"
                 >
                   Asignar
                 </button>
